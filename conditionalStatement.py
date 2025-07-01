a75e024733f82770376156efe48c322c2a099592
Umesh Joshi <uj19343@gmail.com>
2025-07-01 21:20:51 +0545

    conditional and datatype

---
+++ conditionalStatement.py
@@ -0,0 +1,9 @@
+#if-else statement
+
+x = 9
+if x>18:
+    print("eligible to vote")
+elif x<10:
+    print("underaged")
+else:
+    print('overaged')
---
+++ dataType.py
@@ -0,0 +1,5 @@
+name = 'umesh'
+roll = 10
+isStudent = True
+isPassed = False
+print(name,roll,isStudent, isPassed)
