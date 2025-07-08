a224ca8d9485b8678c6f944ab219d42745d36d59
Umesh Joshi <uj19343@gmail.com>
2025-07-08 20:26:25 +0545

    what isi that

--- tuples.py
+++ tuples.py
@@ -1,3 +1,16 @@
 x = ('umesh','ram','gam','jam')
+z = ('chera',)
+# print(x[-1])
+# y = list(x)
+# x[1] = "fam"
+# y[1] ="jammm"
+# x = tuple(y)
+# print(y)
+# print(x)
 
-print(x[-1])
+# y.append("kemxo")
+# x = tuple(y)
+# print(x)
+
+x+=z
+print(x)
